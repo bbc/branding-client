80796484a961e61f1d656b47a5ee6d23d38f5c60
Scott <48245578+bbc-kellys@users.noreply.github.com>
2020-10-27 14:10:52 +0000

    refactor(OrbitClient): Update Forge Environment for Stage (#25)
    refator(OrbitClient): Update Forge Environment for Stage
    
    ## Change
    - updates headers for Orbit Client to have `forge-free` in stage environment
    
    ## JIRA
    https://jira.dev.bbc.co.uk/browse/DATCAP-80

--- src/OrbitClient.php
+++ src/OrbitClient.php
@@ -201,6 +201,7 @@ class OrbitClient
         }
         if ($this->options['env'] === 'stage') {
             $headers['x-forge-environment'] = 'stage';
+            empty($headers['X-Feature']) ? $headers['X-Feature'] = 'forge-free' : $headers['X-Feature'] .= ', forge-free';
         }
         return $headers;
     }
