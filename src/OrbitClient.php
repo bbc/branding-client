1044c46d7b4104e1b9104aec706eeeca8a28e2f1
Scott Kelly <scott.kelly@bbc.co.uk>
2020-11-02 15:14:45 +0000

    updated the check to include forge-free header on stage,int and test

--- src/OrbitClient.php
+++ src/OrbitClient.php
@@ -201,6 +201,8 @@ class OrbitClient
         }
         if ($this->options['env'] === 'stage') {
             $headers['x-forge-environment'] = 'stage';
+        }
+        if ($this->options['env'] !== 'live') {
             empty($headers['X-Feature']) ? $headers['X-Feature'] = 'forge-free' : $headers['X-Feature'] .= ', forge-free';
         }
         return $headers;
