9b088d366259fe25eef4ef7e61746d73b59681a1
Scott Kelly <scott.kelly@bbc.co.uk>
2020-11-02 15:33:49 +0000

    update based on comment

--- src/OrbitClient.php
+++ src/OrbitClient.php
@@ -190,6 +190,7 @@ class OrbitClient
      */
     private function getRequestHeaders(array $options)
     {
+        $envs = ['int', 'stage', 'test'];
         $headers = [
             'Accept' => 'application/ld+json',
             'Accept-Encoding' => 'gzip',
@@ -202,7 +203,7 @@ class OrbitClient
         if ($this->options['env'] === 'stage') {
             $headers['x-forge-environment'] = 'stage';
         }
-        if ($this->options['env'] !== 'live') {
+        if (in_array($this->options['env'], $envs)) {
             empty($headers['X-Feature']) ? $headers['X-Feature'] = 'forge-free' : $headers['X-Feature'] .= ', forge-free';
         }
         return $headers;
