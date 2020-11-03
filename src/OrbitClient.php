af569d9811ac13e7d852d707be1c3e8c27942eab
Scott <48245578+bbc-kellys@users.noreply.github.com>
2020-11-03 11:17:49 +0000

    feat(OrbitClient): Update forge-free header to be on test, int & stage (#26)
    ## Changes
    - Updates the OrbitClient header to be included on all environments except live!
    
    ## Impact
    - Forge free header should appear on other environments.
    
    ## JIRA
    https://jira.dev.bbc.co.uk/browse/DATCAP-80

--- src/OrbitClient.php
+++ src/OrbitClient.php
@@ -201,6 +201,8 @@ class OrbitClient
         }
         if ($this->options['env'] === 'stage') {
             $headers['x-forge-environment'] = 'stage';
+        }
+        if (in_array($this->options['env'], ['int', 'stage', 'test', 'sandbox'])) {
             empty($headers['X-Feature']) ? $headers['X-Feature'] = 'forge-free' : $headers['X-Feature'] .= ', forge-free';
         }
         return $headers;
